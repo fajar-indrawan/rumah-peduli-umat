e4ea4b0fa27798f41ec11af9db12c50a22dca612
Fajar Indrawan <fajar.rahmat.indrawan@gmail.com>
2026-09-10 16:14:04 +0700

    fix CORS config

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -20,7 +20,11 @@ return [
     'allowed_methods' => ['*'],
 
     // 'allowed_origins' => ['http://localhost:3000'],
-    'allowed_origins' => ['*'],
+    // 'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://rumah-peduli-umat.vercel.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
